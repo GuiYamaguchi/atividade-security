29a149ebb5d34147d63cd8309280f8b0b4baff46
Guilherme <japajao25@gmail.com>
2026-05-07 22:14:27 -0300

    repositorio

--- src/main/java/com/ym/atividade/repositories/ProdutoRepository.java
+++ src/main/java/com/ym/atividade/repositories/ProdutoRepository.java
@@ -2,6 +2,8 @@ package com.ym.atividade.repositories;
 
 import com.ym.atividade.models.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>{
 }
--- src/main/java/com/ym/atividade/repositories/UserRepository.java
+++ src/main/java/com/ym/atividade/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.ym.atividade.repositories;
 import com.ym.atividade.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     UserDetails findByLogin(String role);
